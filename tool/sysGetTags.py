c6a26a7a8e59f0bbdb4afa0af0697be3f8860f20
Ted52 <alex.fonrobert@gmx.de>
2018-10-27 09:03:45 +0200

    Revert "Merge branch 'master' of https://gitlab.com/truemikesmith/Modern_Day_4_HOI"
    This reverts commit eb011a4d681a1858b1e6a57629577aa0c8f3fd34, reversing
    changes made to fb0562e9c9dee56084f194cbb534de0eaacada50.

---
+++ tool/sysGetTags.py
@@ -0,0 +1,54 @@
+﻿#!/usr/bin/python
+
+def GetTags(FilePath):
+	TagList = [] #This dictionary will store every tag
+	Success = True
+
+
+	try:
+		with open(FilePath, "r") as File:
+			print("File successfully opened.")
+			for Line in File:
+				if not "#" in Line:
+					Tag = Line[:3]
+					if Tag != "\n":
+						TagList.append(Tag)
+					#print(Tag) #Use for debugging
+	except IOError:
+		print("The file could not be opened")
+		Success = False
+
+	if Success:
+		print("Successfully Created Tag List.")
+		
+	return TagList
+	
+def GetTagDictionary(FilePath):
+	TagList = {} #This dictionary will store every tag
+	Success = True
+
+	try:
+		with open(FilePath, "r") as File:
+			print("File successfully opened.")
+			for Line in File:
+				if not "#" in Line:
+					Tag = Line[:3]
+					FullName = Line[17:]
+					NameLength = 0
+					for Char in FullName:
+						NameLength += 1
+						if Char == ".":
+							NameLength -= 1
+							break
+					FullName = FullName[:NameLength]
+					if not "\n" in Tag:
+						TagList[Tag] = FullName
+				#print(Tag) #Use for debugging
+	except IOError:
+		print("The file could not be opened")
+		Success = False
+
+	if Success:
+		print("Successfully Created Dictionary.")
+		
+	return TagList
