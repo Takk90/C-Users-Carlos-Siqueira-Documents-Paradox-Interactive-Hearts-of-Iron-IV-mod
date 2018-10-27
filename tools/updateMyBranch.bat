661dafd8e9ee2d7f4f7a6f299c1abc5f056e9f92
ColdEvul <andreas.brostrom.ce@gmail.com>
2018-10-27 18:12:00 +0200

    final script update

--- tools/updateMyBranch.bat
+++ tools/updateMyBranch.bat
@@ -1,14 +1,8 @@
 @ECHO OFF
 
+ECHO Updating branch cause i apperently can't do it my self...
 
-ECHO | set /p=Current Branch: 
-git symbolic-ref --short HEAD
-ECHO Updating branch cause i can't do it my self...
-cd ..
-ECHO Updating local reposetory...
-git fetch
-ECHO Rebasing local branch...
-git rebase --abort 
-git rebase master 
-cd tools
-ECHO Compleet.
+
+git fetch -v --progress "origin"
+
+git pull --progress -v --no-rebase "origin" master
